be3ee0b80acd05f722eb0c8a217d2dd9581c6388
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2026-09-01 21:57:35 +0600

    add response env utility

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Application } from "express";
+import v1Routes from "./routes/index";
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.use("/api/v1", v1Routes);
 
 export default app;
